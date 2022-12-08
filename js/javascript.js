dc1461b0b628999aba0f081bc747f4a1e080fd8b
FlorGarc14 <48372089+FlorGarc14@users.noreply.github.com>
2022-12-08 11:01:32 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+
+function modonocturno() { document.body.style.backgroundColor = "black";
+                         document.getElementById('nav').style.backgroundColor= "grey";
+                         document.getElementById('header').style. backgroundColor= "grey";
+ }
+
+function autor () {
+    alert ("Florencia Valentina Garcia correo: garcia.florencia91@gmail.com tel: +5493884761851");
+    }
